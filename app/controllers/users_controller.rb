e1f8c98ef5921fa57c36e37f2a40be4906ab68ba
ayman <aymanjaber2012@gmail.com>
2021-03-14 17:54:09 +0200

    Add functionality to save new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,14 @@ class UsersController < ApplicationController
 
   # POST /users or /users.json
   def create
+
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
  
