09b027d1996a3bd2269017b6a8c8c5ff00524fda
ayman <aymanjaber2012@gmail.com>
2021-03-14 19:55:35 +0200

    Change form to use form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
+    @user = User.new
   end
 
   # POST /users or /users.json
   def create
 
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    # @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
