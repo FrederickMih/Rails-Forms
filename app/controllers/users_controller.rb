53bcd6b632b0412f1dd1fa1f5b8c0a832a8cbda8
ayman <aymanjaber2012@gmail.com>
2021-03-14 21:05:03 +0200

    Add editing capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
   # POST /users or /users.json
   def create
-
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
@@ -18,6 +17,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   private
 
